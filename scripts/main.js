28c53068c0eff3da29f3c3d4b82cf2bed04f4e43
PsychoicCoder <158079502+PsychoicCoder@users.noreply.github.com>
2024-08-31 03:53:45 +0300

    js1

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,5 @@ var myHeading = document.querySelector("h1");
 const url = 'https://img.shields.io/badge/any_text-you_like-blue?style=for-the-badge';
 const response = await fetch(url);
 const text = await response.text();
-
-
-
-
+console.log(text);
 myHeading.textContent = text;
